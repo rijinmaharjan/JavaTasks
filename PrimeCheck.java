e5afa5576a3540899367ab47ed3f80db7c52c9be
Rijin Maharjan <mhzn.rijin8@gmail.com>
2025-04-22 08:22:29 +0545

    Task

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,21 +1,22 @@
-//NEED HELP CANNOT FIGURE IT OUT without using boolean.
-
 import java.util.Scanner;
+
 public class PrimeCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number greater than 1:");
+        System.out.print("Enter a number greater than 1: ");
         int num = input.nextInt();
-        for (int i = 2; i<=num/2; i++) {
+        int count = 0;
+
+        for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
                 System.out.println(num + " is Not a Prime Number.");
+                count = 1;
                 break;
             }
         }
-        else{
+
+        if (count == 0) {
             System.out.println(num + " is a Prime Number.");
         }
     }
-    
 }
-
